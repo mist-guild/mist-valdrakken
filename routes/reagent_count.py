82090380809fafb1c6d50242220ee474a99fb858
Zak Pruitt <pruittzn@gmail.com>
2022-11-27 06:11:38 -0600

    added in updating reagent route

--- routes/reagent_count.py
+++ routes/reagent_count.py
@@ -4,27 +4,49 @@ from models.reagent_count import ReagentCount
 from flask import redirect, request, Blueprint
 
 reagent = Blueprint('reagent', __name__,
-                       url_prefix='/reagent', template_folder='templates')
+                    url_prefix='/reagent', template_folder='templates')
 
 
 @reagent.route('/', methods=['POST'])
 def register_reagent():
     new_reagent = ReagentCount(character_name=request.form["character_name"],
-                                 hochenblume_bronze=0,
-                                 writhebark_gold=0,
-                                 bubble_poppy_bronze=0,
-                                 bubble_poppy_silver=0)
+                               hochenblume_bronze=0,
+                               writhebark_gold=0,
+                               bubble_poppy_bronze=0,
+                               bubble_poppy_silver=0)
     new_reagent.save()
     return "Reagent successfully created!", 201
 
 
 @reagent.route('/<string:character_name>', methods=['GET', 'PUT'])
 def get_put_by_character_name(character_name):
+    reagent = ReagentCount.query.filter_by(
+        character_name=character_name).first()
+
     if request.method == 'GET':
-        reagent = ReagentCount.query.filter_by(
-            character_name=character_name).first()
         return reagent.to_json(), 200
     elif request.method == 'PUT':
-        print(request.json)
-        print("based")
-        return 200
+        # if reagent is None, create reagent
+        if reagent is None:
+            reagent = ReagentCount(character_name=character_name,
+                                   hochenblume_bronze=0,
+                                   writhebark_gold=0,
+                                   bubble_poppy_bronze=0,
+                                   bubble_poppy_silver=0)
+
+        # update reagent
+        del request.json['Character']
+        for data in request.json:
+            setattr(reagent, convert_reagent_name(
+                data), request.json[data])
+        reagent.save()
+        
+        # return reagent
+        return "Reagent successfully saved!", 200
+
+
+# TODO: move to service
+def convert_reagent_name(reagent_name):
+    reagent_name = reagent_name.lower()
+    reagent_name = reagent_name.replace(" - ", "_")
+    return reagent_name
